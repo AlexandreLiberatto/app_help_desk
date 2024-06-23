9779f0b2906592ee5e930365b1ea49bf004ee979
unknown <alexandreliberatto@gmail.com>
2024-06-23 16:36:14 -0300

    Adicionando icone

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -5,6 +5,8 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+    <link rel="shortcut icon" href="logo.png" type="image/png">
+    
     <style>
       .card-consultar-chamado {
         padding: 30px 0 0 0;
